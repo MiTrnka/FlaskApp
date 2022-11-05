397b66968b355f67a44cc2e0088ebcb0cbbea974
Michal Trnka <mitrnka@gmail.com>
2022-11-05 19:33:43 +0100

    Prvni commit

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -5,7 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskApp import app
-
+ 
 @app.route('/')
 @app.route('/home')
 def home():
